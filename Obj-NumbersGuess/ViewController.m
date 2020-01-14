2286102bfe2f2812a9ec4164d3b223f415a08ead
muhannadsayedabdullah <symohannad@gmail.com>
2020-01-14 15:01:04 +0100

    stepper

--- Obj-NumbersGuess/ViewController.m
+++ Obj-NumbersGuess/ViewController.m
@@ -12,10 +12,15 @@
 @property (weak, nonatomic) IBOutlet UILabel *answer;
 @property (weak, nonatomic) IBOutlet UIButton *guessBtn;
 @property (weak, nonatomic) IBOutlet UILabel *pointView;
-@property (weak, nonatomic) IBOutlet UISlider *level;
+
 @property (weak, nonatomic) IBOutlet UILabel *stars;
 @property (weak, nonatomic) IBOutlet UITextField *guessField;
+@property (nonatomic) int point ;
+@property (weak, nonatomic) IBOutlet UIStepper *stepper;
+@property (nonatomic) int level;
+@property (weak, nonatomic) IBOutlet UILabel *levelShow;
 
+- (IBAction)modifyLevel:(id)sender;
 
 @end
 
@@ -24,46 +29,53 @@
 - (IBAction)guessField:(id)sender {
 }
 -(int) randGen {
-    NSUInteger r = arc4random_uniform(10);
+    self.level = _stepper.value;
+    NSUInteger r = arc4random_uniform(self.level);
     NSLog (@"%d" , r);
     return r;
 }
-int point ;
+
 
 
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    
-    
-    _pointView.text =[NSString stringWithFormat:@"Your Point is %i",point];
-    
+
     [_guessBtn addTarget:self action:@selector(buttonPressed:)
      forControlEvents:UIControlEventTouchUpInside];
 
     [self.view addSubview:_guessBtn];
-    
-    
+ 
 }
 
 - (void)buttonPressed:(UIButton *) guessBtn {
      
-    
+    _guessField.text=(@"");
     int hidden =[self randGen];
     int guess = [_guessField.text integerValue];
     
     if (guess == hidden) {
         NSLog  (@"Thanks");
         _answer.text = @"Yes!!! you have right answer";
-        point+1;
+        self.point++;
+     _pointView.text =[NSString stringWithFormat:@"Your Point is %i",self.point];
        // _pointView.text = _pointView
     }else {
         
         _answer.text = @"No!! Try again";
-        point-1;
+        self.point--;
+       _pointView.text =[NSString stringWithFormat:@"Your Point is %i",self.point];
         
     }
 }
 
+- (IBAction)modifyLevel:(id)sender {
+    
+    _levelShow.text = [NSString stringWithFormat: @"Your choosen level is : %i" , self.level];
+    
+    
+    
+    
+}
 @end
